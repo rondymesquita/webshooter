b9af78407e7834c9611c4c46682edd513c41a468
rondymesquita <rondinellimesquitas@gmail.com>
2021-01-05 18:46:58 -0300

    chore: refactor factory to separated methods using literal object

--- src/screenshot/screenshooterFactory.ts
+++ src/screenshot/screenshooterFactory.ts
@@ -7,23 +7,38 @@ import { PaperSize } from '../papersize/papersize'
 import { Orientation } from '../papersize/orientation'
 import { Arguments } from '../args/types'
 
-export default class ScreenshooterFactory {
-  static create(args: Arguments) {
-    const { format, pngMode, pdfPapersize, pdfOrientation } = args
-    let screenshooter
-    if (format === 'PNG') {
-      const options: PNGScreenshooterOptions = {
-        mode: pngMode as PNGScreenshooterMode,
-      }
+type ScreenshooterObjects = {
+  [prop: string]: any
+}
 
-      screenshooter = new PNGScreenshooter(options)
-    } else {
-      const options: PDFScreenshooterOptions = {
-        orientation: pdfOrientation as Orientation,
-        paperSize: pdfPapersize as PaperSize,
-      }
-      screenshooter = new PDFScreenshooter(options)
+export default class ScreenshooterFactory {
+  static createPngScreenshooter(args: Arguments) {
+    const { pngMode } = args
+    const options: PNGScreenshooterOptions = {
+      mode: pngMode as PNGScreenshooterMode,
     }
+
+    return new PNGScreenshooter(options)
+  }
+
+  static createPdfScreenshooter(args: Arguments) {
+    const { pdfPapersize, pdfOrientation } = args
+    const options: PDFScreenshooterOptions = {
+      orientation: pdfOrientation as Orientation,
+      paperSize: pdfPapersize as PaperSize,
+    }
+    return new PDFScreenshooter(options)
+  }
+
+  static create(args: Arguments) {
+    const { format } = args
+
+    const screenshooters: ScreenshooterObjects = {
+      PNG: ScreenshooterFactory.createPngScreenshooter,
+      PDF: ScreenshooterFactory.createPdfScreenshooter,
+    }
+
+    const screenshooter = screenshooters[format](args)
     return screenshooter
   }
 }
