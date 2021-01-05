fef7752f6d0f11b0bc47cca5ff61f36b7f7e298f
rondymesquita <rondinellimesquitas@gmail.com>
2021-01-05 18:56:01 -0300

    feat: add bin property for dist folder and enable cli usage

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
-#!/usr/bin/env ts-node
+#!/usr/bin/env node
 
 import { Arguments } from './args/types'
 import ArgParser from './args/argParser'
 import Webshooter from './webShooter'
 
-
-const args:Arguments = ArgParser.parse(process.argv.slice(2))
-console.log('Args', args);
+const args: Arguments = ArgParser.parse(process.argv.slice(2))
 
 const webshooter = new Webshooter(args)
 webshooter.shot()
